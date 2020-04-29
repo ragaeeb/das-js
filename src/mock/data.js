2cf5406019f73457d91043b23f3fae826098decd
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2020-04-29 18:27:54 -0400

    Add placeholders on first load #16

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,9 +2,9 @@ import { v1 as uuidv1 } from 'uuid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Dār as-Ṣaḥābah',
+  lang: 'en',
+  description: 'Dār as-Ṣaḥābah Association',
 };
 
 // HERO DATA
