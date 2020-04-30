a4a9eaac6e9158dc7fe67f7bcbaf85c8fd102020
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2020-04-29 23:28:40 -0400

    Set up meta tags for website preview #12 (#39)

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,17 @@ export default () => {
     <>
       <Helmet>
         <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:title" content={title || 'Dār as-Ṣaḥābah'} />
+        <meta property="og:description" content={description || 'Dār as-Ṣaḥābah Association'} />
+        <meta name="description" content={description || 'Dār as-Ṣaḥābah Association'} />
+        <meta property="og:url" content="http://dar-as-sahaba.com" />
+        <meta name="twitter:card" content="summary" />
+        <meta property="og:type" content="article" />
+        <meta property="og:locale" content="en_CA" />
+        <link rel="canonical" href="http://dar-as-sahaba.com" />
         <title>{title || 'Dār as-Ṣaḥābah'}</title>
         <html lang={lang || 'en'} />
-        <meta name="description" content={description || 'Dār as-Ṣaḥābah Association'} />
       </Helmet>
       <App />
     </>
