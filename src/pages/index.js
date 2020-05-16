45c2d15f654187f413e84bf8b85dc42e57c2ce39
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2020-05-16 03:05:11 -0400

    Set up meta tags for website preview #12

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import { Helmet } from 'react-helmet';
 import App from '../components/App';
+import SEO from '../components/seo';
 import { headData } from '../mock/data';
 import '../style/main.scss';
 
@@ -10,20 +10,7 @@ export default () => {
 
   return (
     <>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta property="og:title" content={title || 'Dār as-Ṣaḥābah'} />
-        <meta property="og:description" content={description || 'Dār as-Ṣaḥābah Association'} />
-        <meta name="description" content={description || 'Dār as-Ṣaḥābah Association'} />
-        <meta property="og:url" content="http://dar-as-sahaba.com" />
-        <meta name="twitter:card" content="summary" />
-        <meta property="og:type" content="article" />
-        <meta property="og:locale" content="en_CA" />
-        <link rel="canonical" href="http://dar-as-sahaba.com" />
-        <title>{title || 'Dār as-Ṣaḥābah'}</title>
-        <html lang={lang || 'en'} />
-      </Helmet>
+      <SEO title={title} lang={lang} description={description} />
       <App />
     </>
   );
