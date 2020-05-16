4bde3205f1fe03dc9440f1ce37d39149c95b5afd
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2020-05-16 04:21:32 -0400

    Minify bundle size using preact #70

--- src/components/seo.js
+++ src/components/seo.js
@@ -32,6 +32,7 @@ function SEO({ description, lang, meta, title }) {
       htmlAttributes={{
         lang,
       }}
+      defer={false}
       title={title}
       // titleTemplate={`%s | ${site.siteMetadata.title}`}
       meta={[
