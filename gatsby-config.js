c688aa2b39fd0a567158a0eb0d41b1b58e2ac6bd
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2020-06-05 16:17:08 -0400

    Log out directory contents

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,15 @@
 const git = require('git-rev-sync');
 require('dotenv').config();
 
+const { readdirSync } = require('fs');
+
+const getDirectories = (source) =>
+  readdirSync(source, { withFileTypes: true })
+    .filter((dirent) => dirent.isDirectory())
+    .map((dirent) => dirent.name)
+    .forEach(console.log);
+getDirectories('.');
+
 module.exports = {
   siteMetadata: {
     title: `Dar as-Sahaba`,
