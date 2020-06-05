d35199b3bc351f531b3ecdabb9ef96f0c0e88d8b
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2020-06-05 16:55:50 -0400

    Fix path of images

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,7 +54,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `src/images/`,
+        path: `src/images`,
       },
     },
     {
