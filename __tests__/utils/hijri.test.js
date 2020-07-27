1f53d454f3d19c1ea37a892fc6cac701a4ef7b8b
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2020-07-26 22:52:44 -0400

    Add constant in unit-test and detect broken links in markdown (#199)
    * Add constant in unit-test #166
    Add check for detecting broken links #187
    
    * Fix broken links

--- __tests__/utils/hijri.test.js
+++ __tests__/utils/hijri.test.js
@@ -1,35 +1,35 @@
 import { describe, expect, it } from '@jest/globals';
 import hijri from '../../src/utils/hijri';
 
+const EXPECTED_RESULT = {
+  day: 'al-Jumuʿah',
+  month: 'Shawwāl',
+  year: 1441,
+};
+
 describe('hijri', () => {
   describe('writeIslamicDate', () => {
     it('should get the correct hijri date', () => {
       const result = hijri(0, new Date(2020, 5, 5));
       expect(result).toEqual({
+        ...EXPECTED_RESULT,
         date: 14,
-        day: 'al-Jumuʿah',
-        month: 'Shawwāl',
-        year: 1441,
       });
     });
 
     it('should get the correct adjusted hijri date forward', () => {
       const result = hijri(1, new Date(2020, 5, 5));
       expect(result).toEqual({
+        ...EXPECTED_RESULT,
         date: 15,
-        day: 'al-Jumuʿah',
-        month: 'Shawwāl',
-        year: 1441,
       });
     });
 
     it('should get the correct adjusted hijri date backward', () => {
       const result = hijri(-1, new Date(2020, 5, 5));
       expect(result).toEqual({
+        ...EXPECTED_RESULT,
         date: 13,
-        day: 'al-Jumuʿah',
-        month: 'Shawwāl',
-        year: 1441,
       });
     });
   });
--- content/blog/tonights-40-ahadeeth-class-sundayjune-20th-2010-cancelled/index.md
+++ content/blog/tonights-40-ahadeeth-class-sundayjune-20th-2010-cancelled/index.md
@@ -2,7 +2,7 @@
 title: "Tonight's 40 Aḥadīth Class (Sunday, June 20th, 2010)"
 date: '2010-06-20T12:28:54.169Z'
 description: 'Cancelled'
-imageUrl: https://abuaminaelias.com/wp-content/uploads/2019/09/Forty-Hadith-Imam-al-Nawawi.jpg
+imageUrl: https://i0.wp.com/abuaminaelias.com/dailyhadithonline/wp-content/uploads/2019/09/Forty-Hadith-Imam-al-Nawawi.jpg
 thumbnailUrl: https://pbs.twimg.com/media/D3VUJEvXoAA-zFl.png
 ---
 
