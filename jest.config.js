2ea122a7abbfcbce03aa54f8e16c5cea67b75ec2
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2022-12-12 03:42:49 +0400

    Revert "Add back jest configs"
    This reverts commit 2b2d1c4d0eb2b70e346a89f5dc3905650203ef88.

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,11 @@
 // https://stackoverflow.com/questions/56261381/how-do-i-set-a-timezone-in-my-jest-config
 process.env.TZ = 'UTC';
 
+/**
+ * https://www.gatsbyjs.org/docs/unit-testing/
+ */
 module.exports = {
   transform: {
-    "^.+\\.jsx?$": `<rootDir>/jest-preprocess.js`,
-  },
-  moduleNameMapper: {
-    ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
-  },
-  testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`],
-  transformIgnorePatterns: [`node_modules/(?!(gatsby|gatsby-script|gatsby-link)/)`],
-  globals: {
-    __PATH_PREFIX__: ``,
-  },
-  testEnvironmentOptions: {
-    url: `http://localhost`,
-  },
-  setupFiles: [`<rootDir>/loadershim.js`],
-}
+    '^.+\\.jsx?$': `<rootDir>/jest-preprocess.js`,
+  }
+};
